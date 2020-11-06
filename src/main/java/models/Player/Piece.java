ce9fa2a022dd2554509b93f47f4a6159ca85a413
M-Najib Hebrawi <najiphebrawi@hotmail.com>
2020-11-06 15:02:31 +0100

    først commit i piece class

--- src/main/java/models/Player/Piece.java
+++ src/main/java/models/Player/Piece.java
@@ -1,4 +1,37 @@
 package models.Player;
 
+import java.util.Scanner;
+
 public class Piece {
+
+    private boolean bilLock;
+    private boolean racebilLock;
+    private boolean traktorLock;
+    private boolean ufoLock;
+
+
+    public void pieceselect(){
+
+        System.out.println("Tryk 1 for at være Bilen " +
+                "\nTryk 2 for at være Racerbilen " +
+                "\nTryk 3 for at være Traktoren " +
+                "\nTryk 4 for at være UFO'en");
+
+
+        while (true){
+
+            System.out.println("vælg din piece");
+            Scanner scan=new Scanner(System.in);
+            String characterSelect=scan.nextLine();
+
+            if (characterSelect.equals("1")){
+
+            }
+
+        }
+
+
+
+
+    }
 }
