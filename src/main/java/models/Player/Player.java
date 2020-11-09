d95bf15bd1369999f65fc74400fa1dca577eab41
cboej <s205360@student.dtu.dk>
2020-11-09 15:03:20 +0100

    worked on player class.

--- src/main/java/models/Player/Player.java
+++ src/main/java/models/Player/Player.java
@@ -3,11 +3,22 @@ package models.Player;
 public class Player {
     private String name;
     protected int position;
+    private int account;
 
 
-    public Player(String name, int position) {
+    public Player(String name, int position, int account) {
         this.name = name;
         this.position = position;
+        this.account = account;
+    }
+    public void setName(String name){
+        this.name = name;
+
+    }
+    public String getName(){
+        return name;
+
+
     }
 }
 
