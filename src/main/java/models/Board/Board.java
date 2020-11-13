a7f3643af85cb23246644879be60fa8834b3e779
cboej <s205360@student.dtu.dk>
2020-11-13 12:17:36 +0100

    fields in the making

--- src/main/java/models/Board/Board.java
+++ src/main/java/models/Board/Board.java
@@ -4,9 +4,31 @@ public class Board {
     Field[] field = new Field[24]; {
         field[0] = new Field(1 , " welcome to start");
         field[1] = new Field(2 , " ");
-        field [2] = new Field(3 , " ");
+        field[2] = new Field(3 , " ");
         field[3] = new Field(4 , " ");
         field[4] = new Field(5 , " ");
+        field[5] = new Field(6 , " ");
+        field[6] = new Field(5 , " ");
+        field[7] = new Field(8 , " ");
+        field[8] = new Field(9 , " ");
+        field[9] = new Field(10 , " ");
+        field[10] = new Field(11 , " ");
+        field[11] = new Field(12 , " ");
+        field[12] = new Field(13 , " ");
+        field[13] = new Field(14 , " ");
+        field[14] = new Field(15 , " ");
+        field[15] = new Field(16 , " ");
+        field[16] = new Field(17 , " ");
+        field[17] = new Field(18 , " ");
+        field[18] = new Field(19 , " ");
+        field[19] = new Field(20 , " ");
+        field[20] = new Field(21 , " ");
+        field[21] = new Field(22 , " ");
+        field[22] = new Field(23 , " ");
+        field[23] = new Field(24 , " ");
+
+
+
 
 
 
