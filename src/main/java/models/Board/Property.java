83d9b5e58dfc2f32fd8236c271302e6454706215
M-Najib Hebrawi <najiphebrawi@hotmail.com>
2020-11-16 14:04:29 +0100

    29 commit i piece class

--- src/main/java/models/Board/Property.java
+++ src/main/java/models/Board/Property.java
@@ -1,15 +1,15 @@
-package models.Board;
+package models.Fields;
 
 public class Property extends Field {
 
 
-    private int fieldPossion;
+    private int propertyNumber;
     private int propertyPrice;
     private String propertyOwner;
 
-    public Property(int numPosition, int fieldPossion, int propertyPrice, String propertyOwner) {
+    public Property(int numPosition, int propertyNumber, int propertyPrice, String propertyOwner) {
         super(numPosition);
-        this.fieldPossion = fieldPossion;
+        this.propertyNumber = propertyNumber;
         this.propertyPrice = propertyPrice;
         this.propertyOwner = propertyOwner;
     }
