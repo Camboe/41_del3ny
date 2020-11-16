a629318297f786b2dcab911581621353f7bb0a47
Pernille <pernille.lyngholm1@outlook.com>
2020-11-16 14:54:34 +0100

    GUI start

--- src/main/java/View/GuiGame.java
+++ src/main/java/View/GuiGame.java
@@ -1,6 +1,19 @@
 package View;
 
+import gui_fields.GUI_Field;
+import gui_main.GUI;
+
 public class GuiGame {
+
+    private GUI gui;
+    private GUI_Field [] fields;
+
+    public GuiGame (GUI gui){
+        this.gui = gui;
+        this.fields = gui.getFields();
+    }
+
+
 
 
 }
